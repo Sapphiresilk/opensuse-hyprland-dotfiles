c8d4e9dd1315e3934c332d702c6d3eb0a5f0cb53
Dmitriy Goryachev <dmitriy.goryachev@osinit.com>
2024-09-04 22:37:39 +0300

    better hyprland.conf

--- hypr/scripts/updates-check.sh
+++ hypr/scripts/updates-check.sh
@@ -2,19 +2,7 @@
 notify ()
 {
   notify-send "Checking for updates..."
-  notify-send "Updates checked:
-  zypper has $(zypper lu | wc -l) new package(s),
-  flatpak has $(flatpak remote-ls --updates | wc -l) new package(s),
-  brew has $(brew outdated --quiet | wc -l) new package(s).
-  Consider updating sometimes btw.
-  You use openSUSE btw
-  so mind taking manual
-  snapshots before full 
-  system upgrade. 
-
-  Have fun and nice work!
-  Dmitry Goryachev,
-  SUSE team"
+  notify-send "Updates checked: zypper has $(zypper lu | wc -l) new package(s), flatpak has $(flatpak remote-ls --updates | wc -l) new package(s), brew has $(brew outdated --quiet | wc -l) new package(s). Consider updating sometimes btw. You use openSUSE btw so mind taking manual snapshots before full system upgrade. Have fun and nice work! Dmitry Goryachev, SUSE team"
 }
 sleep 60
 notify # at boot
